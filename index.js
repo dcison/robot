0bded78051b80f8406eec0db60679eba6b16854f
dcison <dcison@hotmail.com>
2019-07-19 22:37:09 +0800

    添加周内限制

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const axios = require('axios');
 const {
     requestData,
     url,
-    startTime,
-    endTime,
+    startDay,
+    endDay,
+    startHour,
+    endHour,
     during
-} = require('./config')
+} = require('./config');
+let time = null,
+    day = null,
+    hour = null;
 const canSendMessage = () => {
-    let time = new Date().getHours();
-    console.log(time);
-    time >= startTime && time <= endTime && axios({
+    time = new Date();
+    day = time.getDay();
+    hour = time.getHours();
+    day >= startDay && day <= endDay && hour >= startHour && hour <= endHour && axios({
         method: 'post',
         url,
         headers: {
